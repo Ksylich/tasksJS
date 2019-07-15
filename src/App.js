e380c4b23de77b8f9e1e0dae023c0331c990a87a
ksylich <andrey.okselenko@computools.com>
2019-07-15 12:19:09 +0300

    sixth

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React from 'react';
 import './App.css';
 // import First from './first';
 // import Second from './second';
-import Third from './third';
+// import Third from './third';
 // import Fourth from './fourth';
 // import Fifth from './fifth';
+import Sixth from './sixth';
+
 
 function App() {
   return (
     <div className="App">
       {/* <First/> */}
       {/* <Second/> */}
-      <Third/>
+      {/* <Third/> */}
       {/* <Fourth/> */}
       {/* <Fifth/> */}
+      <Sixth />
       
     </div>
   );
---
+++ src/sixth.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function Node(data, prev) {
+  this.data = data;
+  this.prev = prev;
+}
+
+function getAll(last){
+    let node = last;
+    while(node !== null){
+        console.log('data: ', node.data);
+        console.log('prev: ', node.prev);
+        console.log('');
+        node = node.prev;
+    }
+}
+
+
+
+const Sixth = () => {
+  const n1 = new Node("first", null);
+  const n2 = new Node("first", n1);
+  const n3 = new Node("first", n2);
+  const n4 = new Node("fourth", n3);
+  const n5 = new Node("fifth", n4);
+  const n6 = new Node("fourth", n5);
+  const n7 = new Node("first", n6);
+
+  getAll(n7);
+
+
+
+  return <div>Sixth</div>;
+};
+
+export default Sixth;
